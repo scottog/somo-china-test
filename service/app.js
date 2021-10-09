f9dc31a6fa73f0cc032cae448cba348d702c8244
Scott Gildersleeve <scott.gildersleeve@somoglobal.com>
2021-10-09 19:40:40 -0400

    Adding port

--- service/app.js
+++ service/app.js
@@ -1,13 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
+const port = 9000
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -37,5 +38,9 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.listen(port, () => {
+  console.log(`Test service running on ${port}`)
+})
 
 module.exports = app;
