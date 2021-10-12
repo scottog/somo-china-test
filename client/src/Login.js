7b17025a3660fbb2b41147c529c3f42224bd5c32
Scott Gildersleeve <scott.gildersleeve@somoglobal.com>
2021-10-12 07:21:03 -0400

    Adding stub for login

---
+++ client/src/Login.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { auth, signInWithEmailAndPassword } from "./firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import "./Login.css";
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, loading, error] = useAuthState(auth);
+  const history = useHistory();
+  useEffect(() => {
+    if (loading) {
+      // maybe trigger a loading screen
+      return;
+    }
+    if (user) history.replace("/dashboard");
+  }, [user, loading]);
+  return (
+    <div className="login">
+      <div className="login__container">
+        <div ><h2>TechConnect Network Test</h2></div>
+        <input
+          type="text"
+          className="login__textBox"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="E-mail Address"
+        />
+        <input
+          type="password"
+          className="login__textBox"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <button
+          className="login__btn"
+          onClick={() => signInWithEmailAndPassword(email, password)}
+        >
+          Login to Test
+        </button>
+        {/* <div>
+          <Link to="/reset">Forgot Password</Link>
+        </div> */}
+        {/* <div>
+          Don't have an account? <Link to="/register">Register</Link> now.
+        </div> */}
+      </div>
+    </div>
+  );
+}
+export default Login;
--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, signInWithEmailAndPassword as signIn, signOut } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -17,8 +19,19 @@ const firebaseConfig = {
   measurementId: "G-HJ3VXLJZM8"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const signInWithEmailAndPassword = async (email, password) => {
+  try {
+    await signIn(auth, email, password)
+  } catch (err) {
+    console.error(err)
+    alert(`Login error: ${err}`)
+  }
+}
 
-export { app, analytics }
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebaseApp);
+const db = getFirestore();
+const auth = getAuth();
+
+export { firebaseApp, analytics, db, auth, signInWithEmailAndPassword, signOut }
